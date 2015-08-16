3ce0e87a7a54fcee09f23695a92b63e69f4a76dc
MassivTurtlz <andrewramsier@gmail.com>
2015-08-16 16:14:52 +0100

    rng works. stable build

--- app/src/main/java/com/example/andrew/rpgapp/RandCharBuild.java
+++ app/src/main/java/com/example/andrew/rpgapp/RandCharBuild.java
@@ -11,6 +11,8 @@ import java.util.Random;
 
 /**
  * Created by Andrew on 15-Aug-15.
+ *
+ * SCROLL DOWN LOADS WE ONLY USE THE NAMERETURN CLASS. i left the other one here to show what changed
  */
 public class RandCharBuild {
 
@@ -45,23 +47,64 @@ public class RandCharBuild {
     }
 
 
-    public String testReturn() {
-        return "String";
-    }
 
     public ArrayList nameReturn() {
+
         ArrayList<String> randArray = new ArrayList<>();
+        ArrayList<String> nameArray = new ArrayList<>();
+        //ArrayList<Integer> ageArray = new ArrayList<>();
+        ArrayList<String> classArray = new ArrayList<>();
+        ArrayList<String> raceArray = new ArrayList<>();
+        ArrayList<String> alArray = new ArrayList<>();
+        ArrayList<String> charArray = new ArrayList<>();
+        Random r = new Random();
 
 
-        // you can add to this directly, or just make new batches using this as a template
-        // by the way... when you use this array everything that is typed fills all of the fields.
+        // NAMES
 
-        // MAKE MORE PROFILES!!! THEN ADD A WAY TO RETURN A RANDOM ARRAY, SHOULD NOT BE HARD!
+        nameArray.addAll(Arrays.asList("Steve", "Jordon", "Hector", "Newton", "Leonard", "Jeffery", "Cody", "Brendan", "Jacinto",
+                "Mel", "Mitch", "Theodore", "Patricia", "Gerry", "Felix", "Timmy", "George", "Johnathan", "Benjamin",
+                "Edwin", "Charlie", "Tommie", "Carlo", "Saul", "Carlos", "Leroy", "Courtney", "Gail", "Tyler", "Robert", "Brewhan",
+                "Englebert Humperdink", "Fornos", "Jack", "Kim Jong", "Khalsie"));
+
+        int index = r.nextInt(nameArray.size());
+        String nameVal = nameArray.get(index);
 
 
-        randArray.addAll(Arrays.asList("Uther Lightbringer", "55", "Paladin", "Human", "Lawful Good", "The Alliance"));
+        //AGES 11 THROUGH 110
 
-        return randArray;
+
+        int Low = 11;
+        int High = 110;
+        int ageInt = r.nextInt(High - Low) + Low;
+        String ageVal = "" + ageInt; //cheats lol
+
+        //CLASS HALLS
+
+        classArray.addAll(Arrays.asList("Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Magic-User/Mage/Wizard",
+                "Monk/Mystic", "Paladin", "Ranger", "Sorcerer", "Thief/Rogue", "Warlock", "Warlord/Marshall"));
+
+        index = r.nextInt(classArray.size());
+        String classVal = classArray.get(index);
+
+        //RACISM
+
+        raceArray.addAll(Arrays.asList("Dragonborn", "Drow", "Dwarf", "Eladrin", "Elf / High Elf", "Gnome / Rock Gnome", "Half-Elf", "Half-Orc", "Halfling / Hobbit", "Human", "Tiefling"));
+        index = r.nextInt(raceArray.size());
+        String raceVal = raceArray.get(index);
+
+        //ALLIGNMENT
+
+        alArray.addAll(Arrays.asList("Lawful Good ", "Neutral Good ", "Chaotic Good ", "Lawful Neutral ", "True Neutral ", "Chaotic Neutral ", "Lawful Evil ", "Neutral Evil ", "Chaotic Evil"));
+        index = r.nextInt(alArray.size());
+        String alVal = alArray.get(index);
+
+
+        randArray.addAll(Arrays.asList("Uther Lightbringer", "55", "Paladin", "Human", "Lawful Good"));
+        charArray.addAll(Arrays.asList(nameVal, ageVal, classVal, raceVal, alVal));
+
+
+        return charArray;
     }
 
 
